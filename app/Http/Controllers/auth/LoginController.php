82de39cd3606f2d0a574cef8266d4e47862cdef6
A1ves <ayronwork@gmail.com>
2026-01-03 21:51:54 -0300

    add controllers for dashboard, login, and registration functionality

---
+++ app/Http/Controllers/auth/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+
+class LoginController extends Controller
+{
+    public function index(): View
+    {
+        return view('auth.login');
+    }
+
+public function authenticate(LoginRequest $request)
+{
+    $credentials = $request->only('email', 'password');
+
+    if (Auth::attempt($credentials)) {
+        $request->session()->regenerate();
+        return redirect()->route('dashboard');
+    }
+
+    return back()->withErrors([
+        'email' => 'Ops, alguma informação está errada.',
+    ]);
+}
+}
