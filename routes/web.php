f6f025fa6619e6f3a186e3f57dc55afdd978d181
A1ves <ayronwork@gmail.com>
2026-01-03 21:52:24 -0300

    add login and dashboard routes with authentication middleware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
+use App\Http\Controllers\auth\LoginController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/login', [LoginController::class, 'index'])->name('view.login');
+Route::post('/login', [LoginController::class, 'authenticate'])->name('login');
+
+// Protected routes
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
