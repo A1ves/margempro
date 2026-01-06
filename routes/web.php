57d6d4565d0e49da306aecd87af4ed88c3dd7696
A1ves <ayronwork@gmail.com>
2026-01-06 06:00:31 -0300

    add registration routes and logout functionality

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\auth\LoginController;
+use App\Http\Controllers\auth\RegisterController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
@@ -8,8 +9,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/login', [LoginController::class, 'index'])->name('view.login');
 Route::post('/login', [LoginController::class, 'authenticate'])->name('login');
+Route::get('/register', [RegisterController::class, 'index'])->name('view.register');
+Route::post('/register', [RegisterController::class, 'register'])->name('register');
 
 // Protected routes
 Route::middleware('auth')->group(function () {
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/desenvolvimento', [DashboardController::class, 'development'])->name('development');
 });
